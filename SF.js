ea592df3e2bf89ac60f358308e5ca5ddadb1d368
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2024-01-17 21:21:15 +0800

    Create SF.js

---
+++ SF.js
@@ -0,0 +1,32 @@
+// 2024-01-17
+$task.fetch({
+  url: 'https://ccsp-egmas.sf-express.com/cx-app-video/video/app/video/labelClusterList',
+  method: 'GET',
+  handler: function (response) {
+    $done();
+  }
+});
+
+$task.fetch({
+  url: 'https://ccsp-egmas.sf-express.com/cx-app-base/base/app/ad/queryInfoFlow',
+  method: 'GET',
+  handler: function (response) {
+    $done();
+  }
+});
+
+$task.fetch({
+  url: 'https://ccsp-egmas.sf-express.com/cx-app-base/base/app/ad/queryInfoFlow',
+  method: 'GET',
+  handler: function (response) {
+    $done();
+  }
+});
+
+$task.fetch({
+  url: 'https://ccsp-egmas.sf-express.com/cx-app-base/base/app/bms/queryRecommend',
+  method: 'GET',
+  handler: function (response) {
+    $done();
+  }
+});
