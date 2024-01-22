a8219258e8bea60e91bbde2a7fe7eff24f1d17df
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2024-01-22 10:09:34 +0800

    更新 Soul.js

--- Soul.js
+++ Soul.js
@@ -1,5 +1,9 @@
-//2024-01-22 10:04
+//2024-01-22 10:09
 const url = $request.url;
+const scriptEnvironment = typeof $task != 'undefined' ? 'Surge' : (typeof $loon != 'undefined' ? 'Loon' : (typeof $httpClient != 'undefined' ? 'Qx' : 'Unknown'));
+if (!$response.body || scriptEnvironment === 'Unknown') {
+  $done({});
+}
 let obj = JSON.parse($response.body);
 
 const shouldDeleteData = (url) => {
