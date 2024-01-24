92f569cb155922f380934d96eb9bf2d87abafed8
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2024-01-24 15:08:33 +0800

    Create bili.js

---
+++ bili.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function _0x19e9(_0x511c97,_0x18bc35){const _0x499b0a=_0x499b();return _0x19e9=function(_0x19e9f1,_0x438fcd){_0x19e9f1=_0x19e9f1-0x187;let _0x28a092=_0x499b0a[_0x19e9f1];return _0x28a092;},_0x19e9(_0x511c97,_0x18bc35);}const _0x3c7b45=_0x19e9;(function(_0x25cf8b,_0x4302cf){const _0x5741cb=_0x19e9,_0x337bf3=_0x25cf8b();while(!![]){try{const _0x2074d6=-parseInt(_0x5741cb(0x198))/0x1*(parseInt(_0x5741cb(0x187))/0x2)+-parseInt(_0x5741cb(0x196))/0x3*(-parseInt(_0x5741cb(0x194))/0x4)+-parseInt(_0x5741cb(0x190))/0x5+parseInt(_0x5741cb(0x18f))/0x6+parseInt(_0x5741cb(0x193))/0x7+-parseInt(_0x5741cb(0x18b))/0x8+parseInt(_0x5741cb(0x192))/0x9*(parseInt(_0x5741cb(0x197))/0xa);if(_0x2074d6===_0x4302cf)break;else _0x337bf3['push'](_0x337bf3['shift']());}catch(_0x204005){_0x337bf3['push'](_0x337bf3['shift']());}}}(_0x499b,0xc5c77));let bilibili=$request[_0x3c7b45(0x191)];const authorizationValues=[_0x3c7b45(0x188),_0x3c7b45(0x195)],randomIndex=Math[_0x3c7b45(0x189)](Math[_0x3c7b45(0x18a)]()*authorizationValues[_0x3c7b45(0x18c)]);function _0x499b(){const _0x2aa44a=['random','2990736dNCnpW','length','identify_v1\x20','Authorization','8300664LfHGpR','7308150aFezqr','headers','233811TxdcRI','1970087IKKRho','4868UVsBRe','f4936ef4cfc2bfeefb6ac9b92df54f11CjBVq9ArRyxLQP59ch_-3h2Ykd7Lhwgli3Gz5wew6P7V-OSX-agWBwISd6F6TpgVrvsSVllfODBFWjJOWmxHeVhnYkc1dnMtSFhDZmhnMUxjYUJsdGlsN25xUDNRLTI1SEJxaHRnczJ6cE9RZUp0RVoyckxqWl9QZGFFcm1TM1hrX3RSZ3J2aGFnIIEC','1308RKafSn','730jtHQwM','206627Bfxknm','14IpkAtJ','91f9fa83fe79f57220b8dbc918838512CjBk9QrmHn5lKyGIZPpD4rI1-t-YxTs16kTq9kRsJsynTesnThX2wABdz2lOhbeA1LsSVjBfeXFFUmIxUzR0eFFMZUE1QXdSN3VkMzBFOFE0UFotMHRoVVVDZkF3dzVBdDd3ellNVERzLS1FVkhXQ29Ta25CUnc4VkQ2ZkdRemxQZldnU3AxX2F3IIEC','floor'];_0x499b=function(){return _0x2aa44a;};return _0x499b();}bilibili[_0x3c7b45(0x18e)]=_0x3c7b45(0x18d)+authorizationValues[randomIndex],$done({'headers':bilibili});
