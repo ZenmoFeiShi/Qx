2f1ca1241af672058e0368d87e95dcf09d9d9081
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2024-02-01 11:42:39 +0800

    更新 SF.js

--- SF.js
+++ SF.js
@@ -1,32 +1,8 @@
 // 2024-01-18 18:30
-$task.fetch({
-  url: 'https://ccsp-egmas.sf-express.com/cx-app-video/video/app/video/labelClusterList',
-  method: 'GET',
-  handler: function (response) {
-    $done();
-  }
-});
+^https:\/\/ccsp-egmas\.sf-express\.com\/cx-app-video\/video\/app\/video\/labelClusterList url reject-dict
 
-$task.fetch({
-  url: 'https://ucmp.sf-express.com/proxy/esgcempcore/memberGoods/pointMallService/goodsList',
-  method: 'GET',
-  handler: function (response) {
-    $done();
-  }
-});
+^https:\/\/ucmp\.sf-express\.com\/proxy\/esgcempcore\/memberGoods\/pointMallService\/goodsList url reject-dict
 
-$task.fetch({
-  url: 'https://ccsp-egmas.sf-express.com/cx-app-base/base/app/ad/queryInfoFlow',
-  method: 'GET',
-  handler: function (response) {
-    $done();
-  }
-});
+https://ccsp-egmas.sf-express.com/cx-app-base/base/app/ad/queryInfoFlow url reject-dict
 
-$task.fetch({
-  url: 'https://ccsp-egmas.sf-express.com/cx-app-base/base/app/bms/queryRecommend',
-  method: 'GET',
-  handler: function (response) {
-    $done();
-  }
-});
+https://ccsp-egmas.sf-express.com/cx-app-base/base/app/bms/queryRecommend url reject-dict
