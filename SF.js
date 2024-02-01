7da8a9c77e5e8b38b3209f307b588df480fdff3b
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2024-02-01 11:44:21 +0800

    Delete SF.js

--- SF.js
+++
@@ -1,8 +0,0 @@
-// 2024-01-18 18:30
-^https:\/\/ccsp-egmas\.sf-express\.com\/cx-app-video\/video\/app\/video\/labelClusterList url reject-dict
-
-^https:\/\/ucmp\.sf-express\.com\/proxy\/esgcempcore\/memberGoods\/pointMallService\/goodsList url reject-dict
-
-https://ccsp-egmas.sf-express.com/cx-app-base/base/app/ad/queryInfoFlow url reject-dict
-
-https://ccsp-egmas.sf-express.com/cx-app-base/base/app/bms/queryRecommend url reject-dict
