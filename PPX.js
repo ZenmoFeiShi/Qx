1a155a6bec5b8e0727bf71c608004d937245b0df
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2024-02-02 13:51:19 +0800

    更新 PPX.js

--- PPX.js
+++ PPX.js
@@ -1,4 +1,4 @@
-// 2024-01-28 16:11
+// 2024-02-02 13:51
 const url = $request.url;
 const scriptEnvironment = typeof $task != 'undefined' ? 'Surge' : (typeof $loon != 'undefined' ? 'Loon' : (typeof $httpClient != 'undefined' ? 'Qx' : 'Unknown'));
 
@@ -8,15 +8,17 @@ if (!$response.body || scriptEnvironment === 'Unknown') {
 
 let obj = JSON.parse($response.body);
 
-if (url.includes("/bds/user/check_in/")) {
-  filterProfileEntrances(obj);
-} else if (url.includes("/bds/feed/channel_list/")) {
-  filterChannelModel(obj);
+function filterBannerInfo() {
+  if (obj.data && obj.data.data && Array.isArray(obj.data.data)) {
+    obj.data.data.forEach((item, index) => {
+      if (item.banner_info) {
+        delete obj.data.data[index].banner_info;
+      }
+    });
+  }
 }
 
-$done({ body: JSON.stringify(obj) });
-
-function filterProfileEntrances(obj) {
+function filterProfileEntrances() {
   let profileEntrances = obj.data.profile_entrances;
   let titlesToFilter = ['放心借', '创作中心', '原创特权', '小黑屋', '我的订单'];
 
@@ -24,7 +26,7 @@ function filterProfileEntrances(obj) {
   fixPos(obj.data.profile_entrances);
 }
 
-function filterChannelModel(obj) {
+function filterChannelModel() {
   if (obj.data.channel_model) {
     obj.data.channel_model = obj.data.channel_model.filter(item => ["feed", "image_text"].includes(item.event_name));
     fixPos(obj.data.channel_model);
@@ -36,3 +38,13 @@ function fixPos(arr) {
     arr[i].pos = i + 1;
   }
 }
+
+if (url.includes("/bds/feed/stream/")) {
+  filterBannerInfo();
+} else if (url.includes("/bds/user/check_in/")) {
+  filterProfileEntrances();
+} else if (url.includes("/bds/feed/channel_list/")) {
+  filterChannelModel();
+}
+
+$done({ body: JSON.stringify(obj) });
