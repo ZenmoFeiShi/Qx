1285aa7ab491e16e8f41c8ad1931a52eba52198b
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2024-02-11 17:03:43 +0800

    更新 Smzdm.js

--- Smzdm.js
+++ Smzdm.js
@@ -1,102 +1,121 @@
-// 2024-02-11 01:02
+// 2024-01-19 21:40
 const url = $request.url;
-const body = $response.body;
 
-const recursivelyFilterByCellType = (data) => {
-  if (Array.isArray(data)) {
-    return data.map(recursivelyFilterByCellType).filter(Boolean);
-  } else if (typeof data === 'object' && data !== null) {
-    if (data.cell_type === '23008' || data.cell_type === '23005') {
-      return null;
-    }
-    Object.keys(data).forEach((key) => {
-      data[key] = recursivelyFilterByCellType(data[key]);
-    });
-  }
-  return data;
-};
-
-const fixPos = (arr) => {
-  arr.forEach((item, index) => {
-    item.pos = index + 1;
-  });
-};
-
-const deleteProperties = (paths) => {
-  paths.forEach((path) => {
-    if (getObjectAtPath(obj, path)) {
-      deleteObjectAtPath(obj, path);
-    }
-  });
-};
-
-const getObjectAtPath = (obj, path) => {
-  return path.split('.').reduce((o, p) => o && o[p], obj);
-};
-
-const deleteObjectAtPath = (obj, path) => {
-  const parts = path.split('.');
-  const lastPart = parts.pop();
-  const parent = getObjectAtPath(obj, parts.join('.'));
-  if (parent && parent[lastPart]) {
-    delete parent[lastPart];
-  }
-};
-
-if (!body) {
+if (!$response.body) {
   $done({});
-  return;
 }
 
-let obj = JSON.parse(body);
+let obj = JSON.parse($response.body);
 
-if (url.includes('/v3/home')) {
+if (url.includes("/v3/home")) {
+  const recursivelyFilterByCellType = (data) => {
+    if (Array.isArray(data)) {
+      return data.map(item => recursivelyFilterByCellType(item)).filter(Boolean);
+    } else if (typeof data === 'object') {
+      if (data['cell_type'] === '23008' || data['cell_type'] === '23005') {
+        return null;
+      } else {
+        for (const key in data) {
+          data[key] = recursivelyFilterByCellType(data[key]);
+        }
+        return data;
+      }
+    }
+    return data;
+  };
+  
   obj.data = recursivelyFilterByCellType(obj.data);
 }
 
-const deletePaths = [
-  url.includes('/vip') ? 'data.big_banner' : null,
-  url.includes('/publish/get_bubble') ? 'data' : null,
-  url.includes('/vip/bottom_card_list') ? 'data.rows' : null,
-  url.includes('/util/update') ? 'data.ad_black_list' : null,
-  url.includes('/util/update') ? 'data.operation_float' : null,
-  url.includes('/util/update') ? 'data.haojia_widget' : null,
-  url.includes('/home/list') ? 'data.banner_v2' : null,
-  url.includes('/publish') ? 'data.hongbao' : null,
-].filter(Boolean);
+const fixPos = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].pos = i + 1;
+  }
+};
 
-deleteProperties(deletePaths);
+if (url.includes("/vip") && obj.data.big_banner) {
+  delete obj.data.big_banner;
+}
 
-if (url.includes('/v3/home') && obj.data && obj.data.functions) {
-  obj.data.functions = obj.data.functions.filter((item) => item.type === 'message');
+if (url.includes("/publish/get_bubble") && obj.data) {
+  delete obj.data;
+}
+
+if (url.includes("/v3/home") && obj.data && obj.data.functions) {
+  obj.data.functions = obj.data.functions.filter((item) => item.type === "message");
   fixPos(obj.data.functions);
 }
 
-const servicesFilter = ['articel_manage', '199794', '199796'];
-if (obj.data && obj.data.services) {
-  obj.data.services = obj.data.services.filter((item) => servicesFilter.includes(item.type));
+if (obj && obj.data && obj.data.services) {
+  obj.data.services = obj.data.services.filter((item) => item.type === "articel_manage" || item.type === "199794" || item.type === "199796");
   fixPos(obj.data.services);
 }
 
-if (url.includes('/v3/home') && obj.data && obj.data.component) {
-  const componentFilter = ['circular_banner', 'fixed_banner', 'filter', 'list'];
-  obj.data.component = obj.data.component.filter((item) => componentFilter.includes(item.zz_type));
+if (url.includes("/vip/bottom_card_list") && obj.data.rows) {
+  delete obj.data.rows;
+}
+
+if (url.includes("/v3/home")) {
+  obj.data.component = obj.data.component.filter((item) => 
+    item.zz_type === "circular_banner" || item.zz_type === "fixed_banner" || item.zz_type === "filter" || item.zz_type === "list" 
+  );
   fixPos(obj.data.component);
 }
 
-if (obj.data && obj.data.rows?.length > 0) {
+if (url.includes("/util/update") && obj.data) {
+  if (obj.data.ad_black_list) {
+    delete obj.data.ad_black_list;
+  }
+  
+  if (obj && obj.data && obj.data.operation_float) {
+  delete obj.data.operation_float;
+}
+
+  if (obj.data.haojia_widget) {
+    delete obj.data.haojia_widget;
+  }
+}
+
+
+if (obj && obj.data && obj.data.widget) {
+  delete obj.data.widget;
+}
+
+if (obj && obj.data && obj.data.operation_float_screen) {
+  delete obj.data.operation_float_screen;
+}
+
+if (url.includes("/home/list") && obj.data.banner_v2) {
+  delete obj.data.banner_v2;
+}
+
+if (obj?.data?.rows?.length > 0) {
   obj.data.rows = obj.data.rows.filter(
-    (item) => !item?.ad_banner_id && !['ad_campaign_id_', 'ad_campaign_name', 'abs_position'].includes(item?.ad)
+    (i) => !(i?.hasOwnProperty("ad_banner_id") || ["ad_campaign_id_", "ad_campaign_name", "abs_position"]?.includes(i?.ad))
   );
 }
 
-if (url.includes('/ajax_app/ajax_get_footer_list') && obj.data.activity_banner && obj.data.activity_banner.hot_widget) {
-  obj.data.activity_banner.hot_widget = obj.data.activity_banner.hot_widget.filter((widget) => !widget.pic_url);
+if (url.includes("/publish") && obj.data && obj.data.hongbao) {
+  delete obj.data.hongbao;
 }
 
-if (url.includes('/v1/app/home') && obj.data) {
-  obj.data = obj.data.filter((item) => ['40', '20'].includes(item.id));
-  fixPos(obj.data);
+if (url.includes("/loading") && obj && obj.data) {
+  delete obj.data;
+}
+
+if (url.includes("/ajax_app/ajax_get_footer_list") && obj.data.activity_banner && obj.data.activity_banner.hot_widget) {
+  obj.data.activity_banner.hot_widget.forEach(widget => {
+    if (widget.pic_url) {
+      delete widget.pic_url;
+    }
+  });
+}
+
+if (url.includes("/v1/app/home") && obj.data) {
+  if (obj.data) {
+    obj.data = obj.data.filter((item) => item.id === "40" || item.id === "20");
+    fixPos(obj.data);
+  }
 }
 
 $done({ body: JSON.stringify(obj) });
