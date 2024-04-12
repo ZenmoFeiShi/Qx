27eedb96182913ba1740a97a2b2df652fb3f7f2d
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2024-04-12 20:10:18 +0800

    更新 Keep.js

---
+++ Keep.js
@@ -0,0 +1,32 @@
+//   2024-04-12 20.10
+const url = $request.url;
+let obj = JSON.parse($response.body);
+
+if (url.includes("/config/v3/basic")) {
+  if (obj.data && obj.data.coursePageConfigs && obj.data.coursePageConfigs.courseTabs) {
+    obj.data.coursePageConfigs.courseTabs = obj.data.coursePageConfigs.courseTabs.filter(tab => {
+      return tab.name !== "直播" && tab.name !== "会员";
+    });
+  }
+
+  if (obj.data && obj.data.bottomBarControl && obj.data.bottomBarControl.tabs) {
+    obj.data.bottomBarControl.tabs = obj.data.bottomBarControl.tabs.filter(tab => {
+      return tab.name !== "商城";
+    });
+  }
+}
+
+if (url.includes("/athena/v9/people/my")) {
+  delete obj.data.memberInfo;
+}
+
+if (obj.data && obj.data.groupTags) {
+    obj.data.groupTags = obj.data.groupTags.filter(tagGroup => {
+        return !tagGroup.tags.some(tag => {
+            const tagsToRemove = ["订单与钱包", "我的团队", "帮助中心"];
+            return tagsToRemove.includes(tag.name);
+        });
+    });
+}
+
+$done({ body: JSON.stringify(obj) });
