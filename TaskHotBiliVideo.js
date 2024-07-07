a45e0023198b6df62ceff4cfd92d940cbc6e7ff9
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2024-07-07 12:25:32 +0800

    更新 TaskHotBiliVideo.js

--- TaskHotBiliVideo.js
+++ TaskHotBiliVideo.js
@@ -1,8 +1,11 @@
 /*
 #!name = bilibili星榜全网热播
 #!author = 怎么肥事
-#!update = 2024-07-07 12:20
+#!update = 2024-07-07 12:25
+
+[task_local]
 0 18 * * * https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/TaskHotBiliVideo.js, tag=bilibili星榜, img-url=https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/apps/BiliBili.png, enabled=true
+
 */
 
 const url = `https://api.bilibili.com/pgc/review/gateway/movie/rank/infos?rank_type=COMBINATION&scene_type=1`;
