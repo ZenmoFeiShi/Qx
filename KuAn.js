552e5f4b311a501f15a6bbc752c9b1b4dc9f00cd
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2024-07-29 12:12:15 +0800

    更新 KuAn.js

--- KuAn.js
+++ KuAn.js
@@ -24,12 +24,20 @@ if (url.includes("/v6/account/loadConfig?key=my_page_card_config")) {
         !item.title.includes("红包")
     );
 } else if (url.includes("/v6/main/init")) {
-    obj.data = obj.data.filter(item => ![944, 945].includes(item.entityId) && item.title !== "关注");
-    obj.data.forEach(item => {
-        if (item.entities) {
-            item.entities = item.entities.filter(entity => {
-                return ![2261, 1633, 413, 417, 1754, 1966, 2274, 1170, 1175, 1190, 2258].includes(entity.entityId) && entity.title !== "关注";
+    obj.data.forEach((item) => {
+        if (item.entities && item.entities.length > 0) {
+            item.entities = item.entities.filter((entity) => {
+                return ![2261, 1633, 413, 417, 1754, 1966, 2274, 1170, 1175, 1190, 2258].includes(entity.entityId);
             });
+        }
+    });
+  
+    obj.data = obj.data.filter((item) => {
+        return ![944, 945].includes(item.entityId) && item.title !== "关注";
+    });
+    obj.data.forEach((item) => {
+        if (item.entities) {
+            item.entities = item.entities.filter((entity) => entity.title !== "关注");
         }
     });
 }
