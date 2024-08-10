270dceecfb7e9752fc3f258c194d38c4d3a0ba13
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2024-08-10 15:20:11 +0800

    更新 Pinduoduo.js

--- Pinduoduo.js
+++ Pinduoduo.js
@@ -6,17 +6,11 @@ if (url.includes('/api/alexa/homepage/hub')) {
     if (obj.result) {
         delete obj.result.search_bar_hot_query;
         delete obj.result.icon_set;
-       // delete obj.result.buffer_bottom_tabs;        
-       // delete obj.result.module_order;
-     //   delete obj.result.dy_module; // 百亿补贴 买菜
-      //  delete obj.result.all_top_opts; // 首页顶部tap
-        //delete obj.result.selected_bottom_skin;
-     //   delete obj.result.icon_set_skin;
-
-        // Remove tabs with specific titles
-        obj.result.bottom_tabs = obj.result.bottom_tabs.filter(tab => {
-            return tab.title !== '多多视频' && tab.title !== '大促会场' && tab.title !== '搜索' && tab.title !== '直播';
-        });
+        if (obj.result.bottom_tabs) {
+            obj.result.bottom_tabs = obj.result.bottom_tabs.filter(tab => {
+                return tab.title !== '多多视频' && tab.title !== '大促会场'&& tab.title !== '搜索' && tab.title !== '直播';
+            });
+        }
     }
 }
 
@@ -30,9 +24,13 @@ if (url.includes('/api/philo/personal/hub')) {
 }
 
 if (url.includes("/api/oak/integration/render")) {
-    delete obj.ui.bottom_section;  //限时
-    delete obj.ui.live_section.float_info;  //直播
-    delete obj.bottom_section_list; //限时
+    if (obj.ui) {
+        delete obj.ui.bottom_section; // 限时
+        if (obj.ui.live_section) {
+            delete obj.ui.live_section.float_info; // 直播
+        }
     }
-    
+    delete obj.bottom_section_list; // 限时
+}
+
 $done({body: JSON.stringify(obj)});
