81016e627cdc099aafbd37e5313e2ecaf17aa27f
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2024-08-29 17:10:12 +0800

    更新 TB.js

--- TB.js
+++ TB.js
@@ -1,4 +1,32 @@
-//2024.07.07   01:42
+//2024.08.29   17:10
+
+const targetPaths = [
+  "zone_info.1.commerce",
+  "zone_info.2.banner",
+  "zone_info.0.common_func",
+  "zone_info.3.game",
+  "zone_info.4.auxiliary_func"
+];
+
+function deleteTargetPath(data, path) {
+  let parts = path.split('.');
+  let last = parts.pop();
+  let obj = data;
+  
+  for (let part of parts) {
+    if (obj && obj.hasOwnProperty(part)) {
+      obj = obj[part];
+    } else {
+      return data;
+    }
+  }
+
+  if (obj && obj.hasOwnProperty(last)) {
+    delete obj[last];
+  }
+
+  return data;
+}
 
 const url = $request.url;
 let obj;
@@ -13,41 +41,36 @@ try {
 if (url.includes("/sidebar/home")) {
     delete obj.vip_banner;
     delete obj.tools;
-    }
-    
+}
+
 if (url.includes("/frs/frsBottom")) {
     delete obj.card_activity.small_card;
     delete obj.card_activity.big_card;
     delete obj.ai_chatroom_guide;
-    }
-    
+}
+
 if (url.includes("/user/profile")) {
     delete obj.banner;
     delete obj.duxiaoman_entry;
     delete obj.recom_naws_list;
     delete obj.vip_banner;
     delete obj.namoaixud_entry;
+    
+    targetPaths.forEach(path => {
+        obj = deleteTargetPath(obj, path);
+    });
 }
 
 if (obj.hasOwnProperty("user") && obj["user"].hasOwnProperty("user_growth")) {
     delete obj["user"]["user_growth"];
 }
 
-const typesToRemove = [60, 53,  58, 50, 10, 64, 51, 52, 55, 57, 62];
+const typesToRemove = [60, 53, 58, 50, 10, 64, 51, 52, 55, 57, 62];
 
 if (obj.custom_grid && Array.isArray(obj.custom_grid)) {
     obj.custom_grid = obj.custom_grid.filter(item => !typesToRemove.includes(item.type));
 }
-/*
-if (url.includes('c/s/sync')) {
-    delete obj.floating_icon?.homepage?.icon_url;
-    delete obj.floating_icon?.pb?.icon_url
-    delete obj.mainbar;
-    delete obj.duxiaoman_url;
-    delete obj.whitelist;
-    delete obj.yy_live_tab;
-}
-*/
+
 if (url.includes("/livefeed/feed")) {
     delete obj.data?.banner?.items;
 }
