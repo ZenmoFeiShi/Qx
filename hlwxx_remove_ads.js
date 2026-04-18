d1b4c5cebef92fc5265eea036a6db6af570c88d6
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2026-04-18 16:34:37 +0800

    更新 hlwxx_remove_ads.js

--- hlwxx_remove_ads.js
+++ hlwxx_remove_ads.js
@@ -1,42 +1,41 @@
-
-
 var body = $response.body;
 
 if (!body || (body.indexOf('<!doctype') === -1 && body.indexOf('<!DOCTYPE') === -1 && body.indexOf('<html') === -1)) {
   $done({ body: body });
 } else {
 
-var css = '<style id="hlwxx-adblock">'
-  // 开屏弹窗
+var css = '<style id="hl-adblock">'
   + '#notice_container,.event-notice,.application-popup,'
-  // 底部横条 + 广告栏 + 友情链接
-  + '.addbox,.infomation,.post-content,'
-  // 详情页广告标签+推荐应用+分享横幅
-  + '.list-sec-top,.list-sec,#copy-img,'
-  // 整个 footer（大事记/免费领现金/商务合作/加入我们等）
+  + '.addbox,.download,.infomation,.post-content,'
+  + '.list-sec-top,.list-sec,'
+  + '#copy-img,'
+  + '.slider-banners,'
   + '.footer,'
-  // 统计 iframe
-  + 'iframe[src*="yandex"],iframe[src*="google"]'
-  + '{display:none!important;height:0!important;overflow:hidden!important}'
-  // 列表中广告条目
-  + '.video-item:has(a.gotoclick),.video-item:has(a.tjtagmanager),'
-  + '.video-item:has(a[adv_id]){display:none!important;height:0!important}'
+  + 'iframe[src*="yandex"],iframe[src*="google"],iframe[src*="kwvprfcr"]'
+  + '{display:none!important;height:0!important;overflow:hidden!important;opacity:0!important}'
+  + '.video-item:has(a.gotoclick),'
+  + '.video-item:has(a.tjtagmanager),'
+  + '.video-item:has(a[adv_id])'
+  + '{display:none!important;height:0!important}'
   + '</style>';
 
-var js = '<script id="hlwxx-adblock-js">'
+var js = '<script id="hl-adblock-js">'
   + '!function(){'
-  + 'function c(){'
+  + 'function clean(){'
   + 'document.querySelectorAll(".video-item").forEach(function(e){'
   + 'var a=e.querySelector("a");'
   + 'if(a&&(a.classList.contains("gotoclick")||a.classList.contains("tjtagmanager")||a.hasAttribute("adv_id")))e.remove()});'
   + '["notice_container"].forEach(function(i){var e=document.getElementById(i);if(e)e.remove()});'
-  + '[".application-popup",".addbox",".infomation",".post-content",".list-sec-top",".list-sec","#copy-img",".event-notice",".footer"]'
+  + '[".application-popup",".event-notice",".addbox",".download",".infomation",".post-content",'
+  + '".list-sec-top",".list-sec","#copy-img",".slider-banners",".footer"]'
   + '.forEach(function(s){document.querySelectorAll(s).forEach(function(e){e.remove()})});'
   + 'document.querySelectorAll("script").forEach(function(s){'
   + 'var r=s.getAttribute("src")||"";'
-  + 'if(r.indexOf("stats.kwvprfcr.xyz")>-1||r.indexOf("yandex.ru")>-1||r.indexOf("googletagmanager.com")>-1||r.indexOf("gtag/js")>-1)s.remove()})}'
-  + 'if(document.readyState==="loading")document.addEventListener("DOMContentLoaded",c);else c();'
-  + 'setTimeout(c,1500);setTimeout(c,4000)'
+  + 'if(r.indexOf("stats.kwvprfcr.xyz")>-1||r.indexOf("yandex.ru")>-1||r.indexOf("googletagmanager.com")>-1||r.indexOf("gtag/js")>-1||r.indexOf("cghhqca.cc")>-1)s.remove()});'
+  + 'try{window.plausible=function(){};window.gtag=function(){};window.ym=function(){};window.dataLayer=[]}catch(e){}'
+  + '}'
+  + 'if(document.readyState==="loading")document.addEventListener("DOMContentLoaded",clean);else clean();'
+  + 'setTimeout(clean,1500);setTimeout(clean,4000);setTimeout(clean,8000)'
   + '}();'
   + '<\/script>';
 
