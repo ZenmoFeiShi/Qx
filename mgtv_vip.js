a0fd37129bd4e1bd1555aef447066ff28787a196
ZenmoFeiShi <153728936+ZenmoFeiShi@users.noreply.github.com>
2026-05-14 09:49:46 +0800

    创建 mgtv_vip.js

---
+++ mgtv_vip.js
@@ -0,0 +1,10 @@
+2026/5/14
+
+const TICKET = "6AF89DA4E2103AE383549D6BB890ABE5";
+let url = $request.url;
+if (/[?&]ticket=/.test(url)) {
+  url = url.replace(/([?&]ticket=)[^&]*/i, "$1" + TICKET);
+} else {
+  url += (url.indexOf("?") >= 0 ? "&" : "?") + "ticket=" + TICKET;
+}
+$done({ url });
